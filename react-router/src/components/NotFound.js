b03433c943ab12003159dc16c46c69bd372d809f
g pooja <pooja.g7277@gmail.com>
2024-10-02 14:55:20 +0000

    css and notfication managed

---
+++ react-router/src/components/NotFound.js
@@ -0,0 +1,14 @@
+
+import React from 'react';
+
+const NotFound = () => {
+  return (
+    <div className="not-found">
+      <h2>404 - Not Found</h2>
+      <p>The page you are looking for does not exist.</p>
+      <a href="/" className="btn">Go Back Home</a>
+    </div>
+  );
+};
+
+export default NotFound;
---
+++ react-router/src/components/Notification.js
@@ -0,0 +1,16 @@
+// src/components/Notification.js
+
+import React from 'react';
+
+const Notification = ({ message, onClose }) => {
+  const notificationClass = `notification ${message.type}`;
+
+  return (
+    <div className={notificationClass}>
+      {message.text}
+      <button onClick={onClose} className="btn btn-cancel">Close</button>
+    </div>
+  );
+};
+
+export default Notification;
