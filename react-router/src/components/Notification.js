b9fcf4da02fbf3e7b2733e113eb2df7689034d3d
G Pooja <pooja.g7277@gmail.com>
2024-10-19 12:20:05 +0530

    notification updated

--- react-router/src/components/Notification.js
+++ react-router/src/components/Notification.js
@@ -1,33 +1,14 @@
 import React from 'react';
-import axios from 'axios';
 
-const notification = () => {
-  
-  const API_URL=process.env.REACT_APP_API_URL
-  console.log(API_URL);
+const Notification = ({ message, onClose }) => {
+  const notificationClass = `notification ${message.type}`;
 
- return (
-        <div>
-        <h2>this is the notification page</h2>
-        </div>
-      );
-    };
-    
-    export default notification;
-// // src/components/Notification.js
+  return (
+    <div className={notificationClass}>
+      {message.text}
+      <button onClick={onClose} className="btn btn-cancel">Close</button>
+    </div>
+  );
+};
 
-// import React from 'react';
-
-// const Notification = ({ message, onClose }) => {
-//   const notificationClass = `notification ${message.type}`;
-
-//   return (
-//     <div className={notificationClass}>
-//       {message.text}
-//       <button onClick={onClose} className="btn btn-cancel">Close</button>
-//     </div>
-//   );
-// };
-
-// export default Notification;
-
+export default Notification;
